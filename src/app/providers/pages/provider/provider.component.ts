28ddb8617cbc4f8e515f7d97f2284956600c7341
Diego <diegochoqueramirez@gmail.com>
2021-10-18 02:25:40 -0400

    products and providers updated

--- src/app/providers/pages/provider/provider.component.ts
+++ src/app/providers/pages/provider/provider.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
+import { Product } from 'src/app/products/interfaces/product.interface';
+import { ProductService } from 'src/app/products/services/product.service';
 import { Provider } from '../../interfaces/provider.interface';
 import { ProviderService } from '../../services/provider.service';
 
@@ -11,17 +13,25 @@ import { ProviderService } from '../../services/provider.service';
 })
 export class ProviderComponent implements OnInit {
   provider!: Provider;
+  products: Product[] = [];
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private providerService: ProviderService,
-    private router: Router
+    private router: Router,
+    private productService: ProductService
   ) {}
 
   ngOnInit(): void {
     this.activatedRoute.params
       .pipe(switchMap(({ id }) => this.providerService.getProviderById(id)))
       .subscribe((provider) => (this.provider = provider));
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({ id }) => this.productService.getProductsByProviderId(id))
+      )
+      .subscribe((products) => (this.products = products));
   }
 
   back() {
